457fb1f5aaf2890a292bad5b7e92cd3f4e15b3be
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-08-31 19:12:32 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int x=sc.nextInt();         int y=sc.nextInt();         int s=0;         int c=0;         for(int i=0;i<n;i++)         {             if(a[i]>=x && a[i]<=y)             {                 System.out.format("%d ",a[i]);                 c=1;             }         }         if(c==0)         {             System.out.println("-1");         }
        }
    }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int s=0;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=x && a[i]<=y)
+            {
+                System.out.format("%d ",a[i]);
+                c=1;
+            }
+        }
+        if(c==0)
+        {
+            System.out.println("-1");
+        }
+        
+    }
+}
