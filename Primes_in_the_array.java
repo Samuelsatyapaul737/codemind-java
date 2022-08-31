9e4f4e1b181a44c22f70898ce963be8e356f7641
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-08-31 18:51:21 +0530

    import java.util.*; class Solution {     boolean prime(int n)     {         if(n==1)         {             return false;         }         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         int[] a=new int[n];         int s=0,c=0;         Solution S=new Solution();         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(S.prime(a[i]))             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution 
+{
+    boolean prime(int n)
+    {
+        if(n==1)
+        {
+            return false;
+        }
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int s=0,c=0;
+        Solution S=new Solution();
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(S.prime(a[i]))
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
