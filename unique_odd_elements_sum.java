58244a9c316a7e1897b752d40c6ba027d0c9b4bf
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-08-31 19:24:05 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         int c=0,s=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             for(int j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     a[j]=9999;                 }             }           if(a[i]!=9999 && a[i]%2!=0)           {               s+=a[i];           }         }         System.out.println(s);     } }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int c=0,s=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    a[j]=9999;
+                }
+            }
+          if(a[i]!=9999 && a[i]%2!=0)
+          {
+              s+=a[i];
+          }
+        }
+        System.out.println(s);
+    }
+}
