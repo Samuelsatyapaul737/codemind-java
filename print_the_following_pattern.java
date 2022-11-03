3f6b0b832b477cc057548012660853c33cb518de
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-11-03 11:10:55 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i,j,k;         for(i=n;i>=1;i--)         {           for(j=1;j<i;j++)           {               System.out.printf(" ");           }           for(k=1;k<=n;k++)           {               if(i==1||i==n||k==1||k==n)               {                  System.out.printf("*");               }               else               {                   System.out.printf(" ");               }           }           System.out.printf(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -5,21 +5,26 @@ class Solution
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-            for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
+        int i,j,k;
+        for(i=n;i>=1;i--)
         {
-            if(j==1 ||i==j ||i==n)
-            {
-                System.out.printf("*");
-            }
-            else
-            {
-                System.out.printf(" ");
-            }
-        }
-        System.out.printf("
+          for(j=1;j<i;j++)
+          {
+              System.out.printf(" ");
+          }
+          for(k=1;k<=n;k++)
+          {
+              if(i==1||i==n||k==1||k==n)
+              {
+                 System.out.printf("*");
+              }
+              else
+              {
+                  System.out.printf(" ");
+              }
+          }
+          System.out.printf("
 ");
-    }
+        }
     }
 }
